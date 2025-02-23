8794274c4d724c8a2d293f70b1e8c81e6dbdf5e6
Eric <Eric@DESKTOP-2V4DIDV>
2025-02-23 15:12:07 -0500

    Add project files.

---
+++ ChoreTracker/Models/DataAccess.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.Data.Sqlite;
+
+namespace ChoreTracker.Models
+{
+    public static class DataAccess
+    {
+
+        public static void InitDB()
+        {
+            string FilePath = Path.Combine(Directory.GetCurrentDirectory(), "App_Data/ChoreTracker.db");
+
+            using (var connection = new SqliteConnection($"Data Source={FilePath}"))
+            {
+                connection.Open();
+
+                SqliteCommand cmd = new SqliteCommand(@"CREATE TABLE ""FREQUENCY_TYPE"" (
+	                ""ID""	INTEGER,
+	                ""NAME""	TEXT NOT NULL UNIQUE,
+	                ""DESCRIPTION""	TEXT,
+	                PRIMARY KEY(""ID"" AUTOINCREMENT)
+                )", connection);
+
+                cmd.ExecuteNonQuery();
+
+                cmd = new SqliteCommand(@"CREATE TABLE ""FREQUENCY"" (
+	                ""ID""	INTEGER,
+	                ""NAME""	TEXT NOT NULL UNIQUE,
+	                ""DESCRIPTION""	TEXT,
+	                ""FREQUENCY_TYPE_ID""	INTEGER NOT NULL,
+	                ""FREQUENCY_VALUE""	INTEGER,
+	                PRIMARY KEY(""ID"" AUTOINCREMENT),
+	                CONSTRAINT ""FREQUENCY_FREQUENCY_TYPE"" FOREIGN KEY(""FREQUENCY_TYPE_ID"") REFERENCES ""FREQUENCY_TYPE""(""ID"")
+                )", connection);
+
+                cmd.ExecuteNonQuery();
+
+                cmd = new SqliteCommand(@"CREATE TABLE ""CATEGORY"" (
+	                ""ID""	INTEGER,
+	                ""NAME""	TEXT NOT NULL UNIQUE,
+	                ""DESCRIPTION""	TEXT,
+	                PRIMARY KEY(""ID"" AUTOINCREMENT)
+                )", connection);
+
+                cmd.ExecuteNonQuery();
+
+                cmd = new SqliteCommand(@"CREATE TABLE ""CHORE"" (
+					""ID""	INTEGER,
+					""NAME""	TEXT NOT NULL UNIQUE,
+					""DESCRIPTION""	TEXT,
+					""FREQUENCY_ID""	INTEGER NOT NULL,
+					""CATEGORY_ID""	INTEGER DEFAULT 1,
+					""LAST_DONE""	DATETIME,
+					PRIMARY KEY(""ID"" AUTOINCREMENT),
+					CONSTRAINT ""CHORE_CATEGORY_ID"" FOREIGN KEY(""CATEGORY_ID"") REFERENCES ""CATEGORY""(""ID""),
+					CONSTRAINT ""CHORE_FREQUENCY"" FOREIGN KEY(""FREQUENCY_ID"") REFERENCES ""FREQUENCY""(""ID"")
+				)", connection);
+
+                cmd.ExecuteNonQuery();
+
+                cmd = new SqliteCommand(@"CREATE TABLE ""CHORE_HISTORY"" (
+					""ID""	INTEGER,
+					""CHORE_ID""	INTEGER NOT NULL,
+					""DATETIMESTAMP""	DATETIME NOT NULL,
+					PRIMARY KEY(""ID"" AUTOINCREMENT),
+					CONSTRAINT ""CHORE_HISTORY_CHORE"" FOREIGN KEY(""CHORE_ID"") REFERENCES ""CHORE""(""ID"")
+				)", connection);
+
+                cmd.ExecuteNonQuery();
+
+                cmd = new SqliteCommand(@"INSERT INTO CATEGORY (NAME, DESCRIPTION)
+					VALUES ('UNCATEGORIZED','Chores that have not been assigned any category.')", connection);
+
+                cmd.ExecuteNonQuery();
+
+                cmd = new SqliteCommand(@"INSERT INTO ""FREQUENCY_TYPE"" (NAME, DESCRIPTION)
+					VALUES ('MONTHLY','Occurs on a specific day of the month.'),
+					('DAILY','Occurs after a specific number of days.'),
+					('COUNTER','Used for tracking how often something is done with no scheduled frequency.')", connection);
+
+                cmd.ExecuteNonQuery();
+
+                connection.Close();
+            }
+
+			
+        }
+    }
+}
---
+++ ChoreTracker/Program.cs
@@ -0,0 +1,53 @@
+using ChoreTracker.Models;
+using Microsoft.Data.Sqlite;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+var lifetime = app.Services.GetRequiredService<IHostApplicationLifetime>();
+lifetime.ApplicationStarted.Register(() =>
+{
+    string filePath = Path.Combine(Directory.GetCurrentDirectory(), "App_Data/ChoreTracker.db");
+
+    if (!File.Exists(filePath))
+    {
+        try
+        {
+            DataAccess.InitDB();
+            
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error in creating DB: {ex.Message.ToString()}");
+        }
+
+        Console.WriteLine("Database created and configured.");
+    }
+    else
+    {
+        Console.WriteLine("Database already configured");
+    }
+});
+
+app.Run();
